930968f9c0216e9526ef66a455d0355df2aeab08
jonham <jonham@qq.com>
2021-11-24 15:55:17 +0800

    feat: page add image.onload set RESET_SCALE

--- src/components/bussniess/Viewer/index.tsx
+++ src/components/bussniess/Viewer/index.tsx
@@ -1,7 +1,8 @@
-import React, { memo } from 'react'
+import React, { memo, useRef } from 'react'
 import { useStore } from '@/context'
 import { useDragInfo, useMove } from './hooks'
 import styles from './style.module.scss'
+import { useScale } from '../Controller/hooks'
 
 const Viewer: React.FC = () => {
   const {
@@ -13,12 +14,58 @@ const Viewer: React.FC = () => {
   } = useStore()
   const { isCanDrag } = useDragInfo()
   const { onStartMove, onMoving, onEndMove, offsetPos, dragStatus } = useMove()
+  const { zoomin, zoomout } = useScale()
+  const { dispatch } = useStore()
+  const onDoubleClick = () => {
+    zoomin()
+  }
+  const imgRef = useRef<HTMLImageElement | null>(null)
+  const onImageMetaLoad: React.ReactEventHandler<HTMLImageElement> = () => {
+    const img = imgRef.current
+    if (img) {
+      const ratioX = img.naturalWidth / img.width
+      const ratioY = img.naturalHeight / img.height
+      // SET_RESET_SCALE
+      dispatch({
+        type: 'SET_RESET_SCALE',
+        scale: +Math.max(ratioX, ratioY).toFixed(0)
+      })
+    }
+  }
+
+  const shouldDelay = useRef(false)
+  const onWheel: React.WheelEventHandler<HTMLDivElement> = (ev) => {
+    const abs = Math.abs(ev.deltaY)
+    const ratio = Math.ceil(abs / 8)
+
+    const isOut = ev.deltaY > 0
+    // 判断往上，往下
+    if (abs > 3) {
+      if (shouldDelay.current) return
+
+      shouldDelay.current = true
+      // console.log({ ratio })
+
+      for (let i = 0; i < ratio; i++) {
+        if (isOut) {
+          zoomout()
+        } else {
+          zoomin()
+        }
+      }
+      setTimeout(() => {
+        shouldDelay.current = false
+      }, 100)
+    }
+  }
 
   return (
     <div
       className={styles.imgWrapper}
       onMouseUp={onEndMove}
       onMouseLeave={onEndMove}
+      onWheel={onWheel}
+      onDoubleClick={onDoubleClick}
     >
       <div
         className={styles.container}
@@ -38,6 +85,8 @@ const Viewer: React.FC = () => {
           }}
           src={picturesList[pictureOrder]?.src || ''}
           alt={picturesList[pictureOrder]?.alt || ''}
+          onLoad={onImageMetaLoad}
+          ref={imgRef}
           onMouseDown={onStartMove}
           onMouseMove={onMoving}
         />
